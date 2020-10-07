def27b96a92d67a69474dc3c4106c21f8903e538
aldhairescobar <aldhair.escobar@gmail.com>
2020-10-07 18:12:28 -0500

    addtoCart removeFromCart hover-image-icons

--- src/Context.js
+++ src/Context.js
@@ -3,6 +3,7 @@ const Context = React.createContext();
 
 function ContextProvider(props) {
   const [allPhotos, setAllPhotos] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
   const url =
     "https://raw.githubusercontent.com/aldhairescobar/pictobook-images/main/images.json";
@@ -13,8 +14,41 @@ function ContextProvider(props) {
       .then((data) => setAllPhotos(data));
   }, []);
 
+  function addToCart(img) {
+    setCartItems(() => [...cartItems, img]);
+  }
+
+  function removeCartItem(id) {
+    setCartItems(() => {
+      return cartItems.filter((item) => item.id !== id);
+    });
+  }
+
+  function toggleFavorite(id) {
+    const newPhotosArray = allPhotos.map((photo) => {
+      if (photo.id === id) {
+        return {
+          ...photo,
+          isFavorite: !photo.isFavorite,
+        };
+      }
+      return photo;
+    });
+    setAllPhotos(newPhotosArray);
+  }
+
   return (
-    <Context.Provider value={{ allPhotos }}>{props.children}</Context.Provider>
+    <Context.Provider
+      value={{
+        allPhotos,
+        toggleFavorite,
+        cartItems,
+        addToCart,
+        removeCartItem,
+      }}
+    >
+      {props.children}
+    </Context.Provider>
   );
 }
 
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,11 +1,72 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { Context } from "../Context";
+import PropTypes from "prop-types";
 
 function Image({ img, className }) {
+  const [hovered, setHovered] = useState(false);
+  const { toggleFavorite, addToCart, cartItems, removeCartItem } = useContext(
+    Context
+  );
+
+  function heartIcon() {
+    if (img.isFavorite) {
+      return (
+        <i
+          onClick={() => toggleFavorite(img.id)}
+          className="ri-heart-fill favorite"
+        ></i>
+      );
+    } else if (hovered) {
+      return (
+        <i
+          onClick={() => toggleFavorite(img.id)}
+          className="ri-heart-line favorite"
+        ></i>
+      );
+    }
+  }
+
+  function cartIcon() {
+    const alreadyInCart = cartItems.some((photo) => photo.id === img.id);
+
+    if (alreadyInCart) {
+      return (
+        <i
+          onClick={() => removeCartItem(img.id)}
+          className="ri-shopping-cart-fill cart"
+        ></i>
+      );
+    } else if (hovered) {
+      return (
+        <i
+          onClick={() => addToCart(img)}
+          className="ri-add-circle-line cart"
+        ></i>
+      );
+    }
+  }
+
   return (
-    <div className={`${className} image-container`}>
+    <div
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      className={`${className} image-container`}
+    >
       <img src={img.url} alt="" className="image-grid" />
+      {heartIcon()}
+      {cartIcon()}
     </div>
   );
 }
+
+Image.propTypes = {
+  className: PropTypes.string,
+  img: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    artist: PropTypes.string,
+    isFavorite: PropTypes.bool,
+  }),
+};
 
 export default Image;
